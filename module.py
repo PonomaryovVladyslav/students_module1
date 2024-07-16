ab197fc6368e42d3cfaf88433f85f4f7a8a16f65
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2024-07-16 15:11:17 +0200

    Add file persistence to task operations
    Refactored task operations to now include writing tasks to file and reading from it. A new function to handle output/input to a file has been implemented. Now, every change in the task dictionary including adding, updating and deleting tasks is now stored persistently in 'tasks.txt'. Another function, 'from_file_to_tasks' has been implemented, to load tasks from file when the program starts.

--- module.py
+++ module.py
@@ -1,6 +1,3 @@
-all_tasks = {}
-
-
 def generate_id(tasks: dict) -> int:
     return max(tasks.keys()) + 1 if tasks else 1
 
@@ -16,15 +13,18 @@ def new_task(name: str, description: str, priority: str, status: str) -> dict:
 def add_task(name: str, description: str, priority: str, status: str, tasks: dict) -> None:
     task_id = generate_id(tasks)
     tasks[task_id] = new_task(name, description, priority, status)
+    tasks_to_file(tasks)
 
 
 def update_task(task_id: int, tasks: dict, to_update: dict) -> None:
     tasks[task_id].update(to_update)
+    tasks_to_file(tasks)
 
 
 def delete_task(task_id: int, tasks: dict) -> None:
     if task_id in tasks:
         del tasks[task_id]
+        tasks_to_file(tasks)
     else:
         print(f'Task {task_id} not found')
 
@@ -53,3 +53,30 @@ def get_tasks(tasks: dict, ordering: str | None, search: str | None) -> list[str
     else:
         filtered_tasks = ordering_tasks(search_tasks(tasks, search), ordering)
     return [task_to_string(task, task_id) for task_id, task in filtered_tasks.items()]
+
+
+def tasks_to_file(tasks: dict) -> None:
+    with open('tasks.txt', 'w') as file:
+        for task_id, task in tasks.items():
+            file.write(task_to_string(task, task_id) + '\n')
+
+
+def from_file_to_tasks() -> None:
+    tasks = {}
+    try:
+        with open('tasks.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                task_id, task_info = line.strip().split(': ')
+                task = {'name': (task_info.split(', '))[0], 'description': (task_info.split(', '))[1],
+                        'priority': (task_info.split(', '))[2], 'status': (task_info.split(', '))[3]}
+                tasks[int(task_id)] = task
+    except FileNotFoundError:
+        return tasks
+
+
+def main():
+    all_tasks = from_file_to_tasks()
+
+
+main()
